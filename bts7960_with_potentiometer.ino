914251f3736222d2401e78fd8ea6c6bc1a3b04fc
nitinrameshuf <90519755+nitinrameshuf@users.noreply.github.com>
2024-08-25 17:48:29 -0400

    Create bts7960_with_potentiometer.ino

---
+++ bts7960_with_potentiometer.ino
@@ -0,0 +1,41 @@
+
+#define RPWM 5
+#define LPWM 6
+#define REN 50
+#define LEN 52
+
+
+int pot;
+int out1;
+int out2;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(RPWM,OUTPUT);
+  pinMode(LPWM,OUTPUT);
+  pinMode(LEN,OUTPUT);
+  pinMode(REN,OUTPUT);
+  digitalWrite(REN,HIGH);
+  digitalWrite(LEN,HIGH);
+
+}
+ 
+ 
+void loop() {
+  
+  pot=analogRead(A0);
+  
+  if(pot>512){
+    out1=map(pot,512,1023,0,255);
+    analogWrite(RPWM,out1);
+    analogWrite(LPWM,0);
+    
+  }
+  
+  if(pot<512){
+    out2=map(pot,512,0,0,255);
+    analogWrite(LPWM,out2);
+    analogWrite(RPWM,0);
+    
+  }
+}
