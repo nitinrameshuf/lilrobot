a94f3a0242458d97c1dc74872ec5cc9718e0be78
root <root@ubuntu.mynetworksettings.com>
2025-09-05 03:31:24 +0000

    stuff

--- src/raspberrypi_server/log_backup.sh
+++ src/raspberrypi_server/log_backup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # Backup Sentry Tower Robot  logs to GitHub
 
+# ==== CONFIGURATION ====
+SRC_DIR="$LILROBOT/src"  
+LOG_FILES="/var/log/zabbix/zabbix_server.log /var/log/zabbix/zabbix_agentd.log"
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+
+cd
+
 git add .
 git commit -m "Routine Log Backup"
 git push origin main
